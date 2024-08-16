5c36e624e6c2234f002dc7f3fb061ca0345483da
std-s23007 <takokun1942@gmail.com>
2024-08-16 12:22:27 +0900

    d50Q26 課題

---
+++ d50Q26kadai.java
@@ -0,0 +1,9 @@
+enum Condition {ROAD, MUD, SNOW}
+
+class d50Q26kadai {
+    public static void main(String[] args) {
+        for (int i= 0; i < Condition.values().length; i++) {
+            System.out.println(Condition.values()[i]);
+        }
+    }
+}
